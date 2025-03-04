ea3d733a3708d1d88981bc37fbaaacd9946d2a87
xiaohang <1606769349@qq.com>
2025-03-04 20:52:46 +0800

    add

--- api_chat_stream.go
+++ api_chat_stream.go
@@ -17,22 +17,6 @@ type ChatMessageStreamResponse struct {
 	Answer         string `json:"answer"`
 	CreatedAt      int64  `json:"created_at"`
 	ConversationID string `json:"conversation_id"`
-	Data           struct {
-		Id             string `json:"id"`
-		WorkflowId     string `json:"workflow_id"`
-		SequenceNumber int    `json:"sequence_number"`
-		Inputs         struct {
-			SysQuery          string        `json:"sys.query"`
-			SysFiles          []interface{} `json:"sys.files"`
-			SysConversationId string        `json:"sys.conversation_id"`
-			SysUserId         string        `json:"sys.user_id"`
-			SysDialogueCount  int           `json:"sys.dialogue_count"`
-			SysAppId          string        `json:"sys.app_id"`
-			SysWorkflowId     string        `json:"sys.workflow_id"`
-			SysWorkflowRunId  string        `json:"sys.workflow_run_id"`
-		} `json:"inputs"`
-		CreatedAt int `json:"created_at"`
-	} `json:"data"`
 }
 
 // type ChatMessageStreamResponse struct {
@@ -122,6 +106,7 @@ func (api *API) chatMessagesStreamHandle(ctx context.Context, resp *http.Respons
 				}
 				return
 			} else if resp.Answer == "" {
+				streamChannel <- resp
 				return
 			}
 			streamChannel <- resp
